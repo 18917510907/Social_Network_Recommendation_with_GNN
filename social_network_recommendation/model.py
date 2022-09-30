3e57512cf97e23b027a678b718288b93226294da
18917510907 <64918193+18917510907@users.noreply.github.com>
2022-09-30 10:28:19 -0700

    Update model.py

--- social_network_recommendation/model.py
+++ social_network_recommendation/model.py
@@ -4,6 +4,7 @@ import torch
 class UserFactor(nn.Module):
     '''
     用户潜因子：包括了物品信息聚合和社交信息聚合
+    User latent factor: including item information aggregation and social information aggregation
     '''
     def __init__(self, emb_dim, user_emb, item_emb, rate_emb):
         super(UserFactor, self).__init__()
@@ -33,14 +34,14 @@ class UserFactor(nn.Module):
         self.eps = 1e-10
 
     def forward(self, uids, u_item_pad, u_user_pad, u_user_item_pad):
-        # 物品信息聚合
+        # 物品信息聚合 item information aggregation
         q_a = self.item_emb(u_item_pad[:,:,0])   # B x maxi_len x emb_dim
         mask_u = torch.where(u_item_pad[:,:,0] > 0, torch.tensor([1.], device=self.device), torch.tensor([0.], device=self.device))   # B x maxi_len
         u_item_er = self.rate_emb(u_item_pad[:,:,1])  # B x maxi_len x emb_dim
         
         x_ia = self.g_v(torch.cat([q_a, u_item_er], dim = 2).view(-1, 2 * self.emb_dim)).view(q_a.size())  # B x maxi_len x emb_dim
 
-        ## 注意力机制
+        ## 注意力机制 attention mechanism
         p_i = mask_u.unsqueeze(2).expand_as(x_ia) * self.user_emb(uids).unsqueeze(1).expand_as(x_ia)  # B x maxi_len x emb_dim
         
         alpha = self.user_items_att(torch.cat([x_ia, p_i], dim = 2).view(-1, 2 * self.emb_dim)).view(mask_u.size()) # B x maxi_len
@@ -49,7 +50,7 @@ class UserFactor(nn.Module):
 
         h_iI = self.aggre_items(torch.sum(alpha.unsqueeze(2).expand_as(x_ia) * x_ia, 1))     # B x emb_dim
 
-        # 社交信息聚合
+        # 社交信息聚合 social information aggregation
         q_a_s = self.item_emb(u_user_item_pad[:,:,:,0])   # B x maxu_len x maxi_len x emb_dim
         mask_s = torch.where(u_user_item_pad[:,:,:,0] > 0, torch.tensor([1.], device=self.device), torch.tensor([0.], device=self.device))  # B x maxu_len x maxi_len
         u_user_item_er = self.rate_emb(u_user_item_pad[:,:,:,1]) # B x maxu_len x maxi_len x emb_dim
@@ -66,14 +67,14 @@ class UserFactor(nn.Module):
 
         h_oI = self.aggre_items(h_oI_temp.view(-1, self.emb_dim)).view(h_oI_temp.size())  # B x maxu_len x emb_dim
 
-        ## 注意力机制
+        ## 注意力机制 attention mechanism
         beta = self.user_users_att(torch.cat([h_oI, self.user_emb(u_user_pad)], dim = 2).view(-1, 2 * self.emb_dim)).view(u_user_pad.size())
         mask_su = torch.where(u_user_pad > 0, torch.tensor([1.], device=self.device), torch.tensor([0.], device=self.device))
         beta = torch.exp(beta) * mask_su
         beta = beta / (torch.sum(beta, 1).unsqueeze(1).expand_as(beta) + self.eps)
         h_iS = self.aggre_neigbors(torch.sum(beta.unsqueeze(2).expand_as(h_oI) * h_oI, 1))     # B x emb_dim
 
-        ## 使用mlp将两者进行结合
+        ## 使用mlp将两者进行结合 combine these two with mlp
         h_i = self.combine_mlp(torch.cat([h_iI, h_iS], dim = 1))
 
         return h_i
@@ -82,6 +83,7 @@ class UserFactor(nn.Module):
 class ItemFactor(nn.Module):
     '''
     物品潜因子
+    item potential factor
     '''
     def __init__(self, emb_dim, user_emb, item_emb, rate_emb):
         super(ItemFactor, self).__init__()
@@ -99,14 +101,14 @@ class ItemFactor(nn.Module):
         self.eps = 1e-10
 
     def forward(self, iids, i_user_pad):
-        # 用户信息聚合
+        # 用户信息聚合 user information aggregation
         p_t = self.user_emb(i_user_pad[:,:,0])
         mask_i = torch.where(i_user_pad[:,:,0] > 0, torch.tensor([1.], device=self.device), torch.tensor([0.], device=self.device))
         i_user_er = self.rate_emb(i_user_pad[:,:,1])
         
         f_jt = self.g_u(torch.cat([p_t, i_user_er], dim = 2).view(-1, 2 * self.emb_dim)).view(p_t.size())
         
-        # 注意力机制
+        # 注意力机制 attention mechanism
         q_j = mask_i.unsqueeze(2).expand_as(f_jt) * self.item_emb(iids).unsqueeze(1).expand_as(f_jt)
         
         miu = self.item_users_att(torch.cat([f_jt, q_j], dim = 2).view(-1, 2 * self.emb_dim)).view(mask_i.size())
@@ -147,7 +149,7 @@ class GNN(nn.Module):
         h_i = self.user_model(uids, u_item_pad, u_user_pad, u_user_item_pad)
         z_j = self.item_model(iids, i_user_pad)
 
-        # 评分预测
+        # 评分预测 evaluation prediction
         r_ij = self.rate_pred(torch.cat([h_i, z_j], dim = 1))
 
         return r_ij
